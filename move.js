1ddf84c826286f73e3f3954beb8c1fdf12578677
cxx1357220 <515302174@qq.com>
2025-06-14 14:54:51 +0800

    fix: move

--- move.js
+++ move.js
@@ -1,11 +1,11 @@
 const fs = require('fs')
-fs.rm('../inb-viewer/shareHtml', {
+fs.rm('../inb-viewer/public/shareHtml', {
     recursive: true
 }, (err) => {
     if (err) {
         console.log('err: ', err);
     } else {
-        fs.cp('./dist', '../inb-viewer/shareHtml', {
+        fs.cp('./dist', '../inb-viewer/public/shareHtml', {
             recursive: true
         }, (err) => {
             if (err) {
